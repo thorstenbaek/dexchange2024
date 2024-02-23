8baa1815f18ea14e39a98995c8657d19deb87424
thorstenbaek <tst@dips.no>
2024-02-23 18:38:03 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1>d:exchange 2024</h1>
+<ul>
+    <li>onsdag 17. april</li>
+    <li>torsdag 18. april</li>
+</ul>
---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+import azure from 'svelte-adapter-azure-swa';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: azure()
+	}
+};
+
+export default config;
