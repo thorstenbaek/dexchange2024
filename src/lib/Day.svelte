8fa1b90680c2e4fd521b780ed4b7b8e69afa9094
thorstenbaek <tst@dips.no>
2024-03-21 18:49:05 +0100

    Added logo Adjusted colors

--- src/lib/Day.svelte
+++ src/lib/Day.svelte
@@ -7,8 +7,32 @@
 
     $: $dayIndexStore = day;
 </script>
+<nav class="days">
+    <a class={$dayIndexStore == 0 ? 'selected':''} href="./day1">Onsdag</a>
+    |
+    <a class={$dayIndexStore == 1 ? 'selected':''} href="./day2">Torsdag</a>
+</nav>
 {#await loadAll([scheduleStore])}
     Loading Schedule...
 {:then}    
     <Schedule/>
 {/await}
+
+<style>
+    .days {
+        font-size: 1.5rem;
+        background: var(--accent-0);
+        color: var(--accent-1);
+        padding: 0 15px;
+    }
+
+    a {
+        color: var(--accent-1);
+        font-weight: bold;
+        text-decoration: none;    
+    }
+
+    .selected {
+        color: white;
+    }
+</style>
